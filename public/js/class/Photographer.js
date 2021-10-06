098299ae775231d3c7f90d2cd0d748e85576eb6b
Julie Moreau <julie_bonnet_01@hotmail.com>
2021-10-06 17:19:24 +0200

    FIX BUG

--- public/js/class/Photographer.js
+++ public/js/class/Photographer.js
@@ -16,16 +16,16 @@ export class Photographer {
   creatHtmlIndex () {
     return `
     <section class="index ${this.classTag}">
-    <a href="photographer.html?id=${this.id}" aria-label="Galerie des travaux de ${this.name}" tabindex="5">
-      <figure>
-        <img src="public/images/photographers_id_photos/${this.portrait}" alt="" />
-      </figure>
-      <h2>${this.name}</h2>
-    </a>
-    <p class="index_country" tabindex="5">${this.city}, ${this.country}</p>
-    <p class="index_tagline" tabindex="5">${this.tagline}</p>
-    <p class="index_price" tabindex="5">${this.price} €/jour</p>
-    <ul>${this.liTags}</ul>
+      <a href="photographer.html?id=${this.id}" aria-label="Galerie des travaux de ${this.name}" tabindex="5">
+        <figure>
+          <img src="public/images/photographers_id_photos/${this.portrait}" alt="" />
+        </figure>
+        <h2>${this.name}</h2>
+      </a>
+      <p class="index_country" tabindex="5">${this.city}, ${this.country}</p>
+      <p class="index_tagline" tabindex="5">${this.tagline}</p>
+      <p class="index_price" tabindex="5" aria-label="${this.price} € par jour">${this.price} €/jour</p>
+      <ul>${this.liTags}</ul>
     </section>
     `
   }
@@ -49,11 +49,11 @@ export class Photographer {
   // Template Vignette Price
   creatHtmlPhotographerPrice () {
     return `
-      <span tabindex="8" class="like">
-        <span id="total_like"></span> 
-        <i class="fas fa-heart" aria-hidden="true"></i>
-      </span>
-      <span tabindex="8">${this.price}€ / jour</span>
+    <span tabindex="8" class="like">
+      <span id="total_like"></span> 
+      <i class="fas fa-heart" aria-hidden="true"></i>
+    </span>
+    <span tabindex="8" aria-label="${this.price} € par jour">${this.price}€ / jour</span>
     `
   }
 }
